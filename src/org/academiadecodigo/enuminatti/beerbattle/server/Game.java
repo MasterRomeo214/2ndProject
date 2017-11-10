7a4d51d31c18f20905a4522816dcc92a8b472d04
Ivan <ivanbsilvestre@gmail.com>
2017-11-10 20:59:48 +0000

    Server skeletor

--- src/org/academiadecodigo/enuminatti/beerbattle/server/Game.java
+++ src/org/academiadecodigo/enuminatti/beerbattle/server/Game.java
@@ -1,10 +1,50 @@
 package org.academiadecodigo.enuminatti.beerbattle.server;
 
+import java.net.Socket;
+
+
 /**
  * Created by codecadet on 07/11/17.
  */
 public class Game {
 
+    private Socket SocketOne;
+    private Socket SocketTwo;
+
+    private Boats[] boatsPlayerOne;
+    private Boats[] BoatsPlayerTwo;
+
+    public Game(Boats[] boatsPlayerOne, Boats[] boatsPlayerTwo) {
+        this.SocketOne = new Socket();
+        this.SocketTwo = new Socket();
+        this.boatsPlayerOne = boatsPlayerOne;
+        BoatsPlayerTwo = boatsPlayerTwo;
+    }
 
 
+    public void receiveSockets(Socket socket) {
+
+    }
+
+    public void receiveAttacks() {
+
+    }
+
+    public void respondAttacks() {
+
+    }
+
+
+    public void receiveLoser() {
+
+    }
+
+
+    public Boats[] getBoatsPlayerOne() {
+        return boatsPlayerOne;
+    }
+
+    public Boats[] getBoatsPlayerTwo() {
+        return BoatsPlayerTwo;
+    }
 }
--- src/org/academiadecodigo/enuminatti/beerbattle/server/Server.java
+++ src/org/academiadecodigo/enuminatti/beerbattle/server/Server.java
@@ -1,17 +1,16 @@
 package org.academiadecodigo.enuminatti.beerbattle.server;
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.LinkedList;
+
 
 /**
  * Created by codecadet on 07/11/17.
  */
 public class Server {
+
 
     public static void main(String[] args) {
 
@@ -19,116 +18,47 @@ public class Server {
 
         try {
             server = new Server();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         server.init();
     }
 
-    private ServerSocket serverSocket;
-    private int portNumber = 8080;
-    private LinkedList<ServerWorker> clients;
-    int connectionCount = 0;
 
+    private ServerSocket serverSocket;
+    public final static int PORTNUMBER = 8080;
 
 
     public Server() throws IOException {
-        clients = new LinkedList<>();
-        serverSocket = new ServerSocket(portNumber);
-        System.out.println("waiting client " + serverSocket.getLocalPort());
 
+        serverSocket = new ServerSocket(PORTNUMBER);
+        System.out.println("Waiting player " + serverSocket.getLocalPort());
     }
 
 
     private void init() {
+
+        int connectionCount = 0;
+
+
         while (true) {
 
+            Socket socket = null;
 
             try {
-                Socket socket = serverSocket.accept();
+                socket = serverSocket.accept();
                 connectionCount++;
-                System.out.println("Client Connected!");
-                ServerWorker serverWorker = new ServerWorker(socket);
-                clients.add(serverWorker);
-                new Thread(serverWorker).start();
+                System.out.println("Player " + connectionCount + " is connected!");
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-
-
-    public void broadcast(String b, ServerWorker serverWorker) {
-
-        for (ServerWorker client : clients) {
-            if (client == serverWorker) {
-                continue;
-            }
-            client.printMessage(b);
-        }
-    }
-
-    public class ServerWorker implements Runnable {
-
-        private BufferedReader bufferedReader;
-        private PrintWriter printWriter;
-        private final Socket socket;
-
-        public ServerWorker(Socket socket) {
-            this.socket = socket;
-            bufferedReader = null;
-            printWriter = null;
-        }
-
-
-        public String getMessage() {
-
-            String b = null;
-
-            try {
-                bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                b = bufferedReader.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return b;
-        }
-
-        public void printMessage(String b) {
-            try {
-                printWriter = new PrintWriter(socket.getOutputStream(), true);
-                printWriter.println(b);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void disconnect() {
-
-            try {
-                bufferedReader.close();
-                printWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-        @Override
-        public void run() {
-
-            while (true) {
-                String message = getMessage();
-                broadcast(message, this);
-            }
-        }
-    }
-
-
-
-
-
-
-
 }
+
+
+
+
+
