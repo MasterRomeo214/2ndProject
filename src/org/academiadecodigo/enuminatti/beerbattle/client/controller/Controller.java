02fc36646ad7af27f5e458628a95b856382554dd
Goncalo Carrondo <filipecarrondo94@gmail.com>
2017-11-11 17:52:09 +0000

    methods for getting x and y position through event done

--- src/org/academiadecodigo/enuminatti/beerbattle/client/controller/Controller.java
+++ src/org/academiadecodigo/enuminatti/beerbattle/client/controller/Controller.java
@@ -1,24 +1,51 @@
 package org.academiadecodigo.enuminatti.beerbattle.client.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import org.academiadecodigo.enuminatti.beerbattle.client.service.ComunicationService;
 
 public class Controller {
 
+
     private ComunicationService comunicationService;
 
-    public void play(){
+    @FXML
+    void buttonPressed(ActionEvent event) {
+
+        Button bClicked = (Button) event.getSource();
+
+        System.out.println(bClicked);
+        System.out.println(getPositionX(bClicked));
+        System.out.println(getPositionY(bClicked));
 
     }
 
-    public void events(){
+
+    public void play() {
 
     }
 
-    public void getPosition(){
+    public void events() {
 
     }
 
-    public void gameLost(){
+    public Integer getPositionX(Button button6) {
+
+        Integer x = GridPane.getColumnIndex(button6);
+        return (x == null) ? 0 : x;
+
+    }
+
+    public Integer getPositionY(Button button6) {
+
+        Integer y = GridPane.getRowIndex(button6);
+        return (y == null) ? 0 : y;
+
+    }
+
+    public void gameLost() {
 
     }
 }
